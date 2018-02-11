aaf3c10217f16f8006c0707136a398da85125e28
yannicklerestif <yannick1976.git@gmail.com>
2018-02-11 15:26:02 +0100

    add material. setup sass. add router. add components and services. add header bar

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UIRouterModule } from '@uirouter/angular';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { CountryService } from "./services/country.service";
 import { AppComponent } from './app.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { DetailsComponent } from './components/details/details.component';
 
+import { detailsState, pricesState} from './states';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PricesComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIRouterModule.forRoot({
+      states: [ detailsState, pricesState ], // FIXME set up
+      useHash: true,
+    }),
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    CountryService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
