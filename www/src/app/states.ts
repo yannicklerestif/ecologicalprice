d3e310f1b069ee70fa36ffdfa9976457ae8eabda
yannicklerestif <yannick1976.git@gmail.com>
2018-02-17 11:31:28 +0100

    router stuff + header styling

--- www/src/app/states.ts
+++ www/src/app/states.ts
@@ -15,34 +15,13 @@ import { ViewportForwarderComponent } from './components/viewport-forwarder/view
 /** UIRouter Config  */
 export function uiRouterConfigFn(router: UIRouter, injector: Injector) {
   let countryService: CountryService = injector.get(CountryService);
-  router.urlService.rules.otherwise({ state: 'root.prices' });
-  router.transitionService.onFinish({}, (transition: Transition) => {
-    let countryCodeParam = transition.params().country;
-    if (countryService.isValidCountryCode(countryCodeParam)) {
-      countryService.setSelectedCountryCode(countryCodeParam);
-      console.log(`-- country code is valid: ${countryCodeParam}`);
-      return true;
-    }
-    let selectedCountryCode = countryService.getDefaultCountryCode();
-    const redirectState = transition
-      .targetState()
-      .withParams({ country: selectedCountryCode })
-      .withOptions({ location: false });
-
-    console.log(
-      `invalid country code: ${countryCodeParam} => will replace with ${selectedCountryCode}`
-    );
-    router.stateService.go(
-      redirectState.name(),
-      redirectState.params(),
-      redirectState.options()
-    );
-  });
+  router.urlService.rules.otherwise({ state: 'root.prices.summary' });
 }
 
 export const states: Ng2StateDeclaration[] = [
   {
     name: 'root',
+    url: '',
     abstract: true,
     resolve: [
       {
@@ -61,17 +40,46 @@ export const states: Ng2StateDeclaration[] = [
   },
   {
     name: 'root.prices',
-    url: '/prices/?country',
-    params: {
-      country: {
-        dynamic: true,
+    url: 'prices?country',
+    abstract: true,
+    resolve: [
+      {
+        token: 'country',
+        deps: ['countries', CountryService, Transition, StateService],
+        resolveFn: (
+          countries,
+          countryService: CountryService,
+          transition: Transition,
+          stateService: StateService
+        ) => {
+          let countryCodeParam = transition.params().country;
+          if (countryService.isValidCountryCode(countryCodeParam)) {
+            countryService.setSelectedCountryCode(countryCodeParam);
+            return;
+          }
+          let selectedCountryCode = countryService.getDefaultCountryCode();
+          const redirectState = transition
+            .targetState()
+            .withParams({ country: selectedCountryCode })
+            .withOptions({ location: true });
+          stateService.go(
+            redirectState.name(),
+            redirectState.params(),
+            redirectState.options()
+          );
+        },
       },
-    },
+    ],
+    component: ViewportForwarderComponent,
+  },
+  {
+    name: 'root.prices.summary',
+    url: '/summary',
     component: PricesComponent,
   },
   {
-    name: 'root.details',
-    url: '/details/?country',
+    name: 'root.prices.details',
+    url: '/details',
     component: DetailsComponent,
   },
 ];
